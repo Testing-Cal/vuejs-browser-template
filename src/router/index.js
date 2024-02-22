70e05427d9aa8cea9ad53b9cb573b9a4114a6a78
ashende@calibo.com <ashende@calibo.com>
2024-02-22 10:08:07 +0000

    Yd/context path

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
-const routes = []
+import HomePage from '@/views/HomePage.vue';
+import DashboardPage from '@/views/DashboardPage.vue';
+import ListingPage from '@/views/ListingPage.vue';
+import PageNotFound from '@/views/PageNotFound.vue';
+
+const CONTEXT_PATH =  process.env.VUE_APP_CONTEXT_PATH
+console.log(CONTEXT_PATH)
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/dashboard', component: DashboardPage },
+  { path: '/listing', component: ListingPage },
+  { path: '/:pathMatch(.*)*', component: PageNotFound },
+];
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(CONTEXT_PATH),
   routes
 })
 export default router
